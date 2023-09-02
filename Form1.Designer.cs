b5436a84b100fb61b43803143165fe8f11c9532f
Piarre <pierre@ideestore.fr>
2023-09-02 12:20:32 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Temped
+{
+    partial class Temped
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enableTempFolder = new System.Windows.Forms.Button();
+            this.githubProject = new System.Windows.Forms.LinkLabel();
+            this.checkTask = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enableTempFolder
+            // 
+            this.enableTempFolder.Location = new System.Drawing.Point(149, 106);
+            this.enableTempFolder.Name = "enableTempFolder";
+            this.enableTempFolder.Size = new System.Drawing.Size(117, 38);
+            this.enableTempFolder.TabIndex = 0;
+            this.enableTempFolder.Text = "Create task";
+            this.enableTempFolder.UseVisualStyleBackColor = true;
+            this.enableTempFolder.Click += new System.EventHandler(this.enableTempFolder_Click);
+            // 
+            // githubProject
+            // 
+            this.githubProject.AutoSize = true;
+            this.githubProject.Location = new System.Drawing.Point(13, 167);
+            this.githubProject.Name = "githubProject";
+            this.githubProject.Size = new System.Drawing.Size(73, 13);
+            this.githubProject.TabIndex = 3;
+            this.githubProject.TabStop = true;
+            this.githubProject.Text = "Github project";
+            this.githubProject.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.githubProject_LinkClicked);
+            // 
+            // checkTask
+            // 
+            this.checkTask.Location = new System.Drawing.Point(17, 106);
+            this.checkTask.Name = "checkTask";
+            this.checkTask.Size = new System.Drawing.Size(117, 38);
+            this.checkTask.TabIndex = 4;
+            this.checkTask.Text = "Check for task";
+            this.checkTask.UseVisualStyleBackColor = true;
+            this.checkTask.Click += new System.EventHandler(this.checkTask_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Temped
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 192);
+            this.Controls.Add(this.checkTask);
+            this.Controls.Add(this.githubProject);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enableTempFolder);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Temped";
+            this.Text = "Temped";
+            this.Load += new System.EventHandler(this.Temped_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enableTempFolder;
+        private System.Windows.Forms.LinkLabel githubProject;
+        private System.Windows.Forms.Button checkTask;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32.TaskScheduler;
+
+namespace Temped
+{
+    public partial class Temped : Form
+    {
+        String TASK_NAME = "Temp folder (DO NOT CHANGE TASK'S NAME)";
+        String TASK_DESCRIPTION = "Created by Temped (https://github.com/Piarre/Temped)";
+
+        public Temped()
+        {
+            InitializeComponent();
+        }
+        private void Temped_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void enableTempFolder_Click(object sender, EventArgs e)
+        {
+            if (!CreateTempFolder()) return;
+            CreateAndScheduleTask();
+        }
+
+        private Boolean CreateTempFolder()
+        {
+            string folderPath = @"%USERPROFILE%\Downloads\Temp"; 
+
+            if (!Directory.Exists(folderPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(folderPath);
+                    Console.WriteLine("Folder created");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error while created the temp folder : {ex.Message}");
+                    return false;
+                }
+            }
+            else
+            {
+                var boxResult = MessageBox.Show("A folder with this name already exists, overwrite it ?", null, MessageBoxButtons.OKCancel);
+
+                if (boxResult == DialogResult.OK)
+                {
+                    Directory.Delete(folderPath, true);
+                    Directory.CreateDirectory(folderPath);
+                    return true;
+                } else if (boxResult == DialogResult.Cancel)
+                {
+                    return false;
+                }
+               
+                return false;
+            }
+        }
+
+        private void CreateAndScheduleTask()
+        {
+            string tempFolderPath = @"%USERPROFILE%\Downloads\Temp\*";
+
+            if (TaskExists(TASK_NAME))
+            {
+                MessageBox.Show("The task is already exists, overwrite it ?");
+                return;
+            }
+
+            using (TaskService ts = new TaskService())
+            {
+                TaskDefinition td = ts.NewTask();
+                td.Actions.Add(new ExecAction("powershell.exe", $"-NoProfile -ExecutionPolicy Bypass -File Remove-Item -Path \"{tempFolderPath}\" -Recurse", null));
+
+                td.RegistrationInfo.Author = "Piarre";
+                td.RegistrationInfo.Date = DateTime.Parse("02/09/2023");
+                td.RegistrationInfo.Description = TASK_DESCRIPTION;
+                td.Settings.Hidden = false;
+                td.Settings.AllowDemandStart = false;
+
+                td.Triggers.Add(new LogonTrigger());
+
+                ts.RootFolder.RegisterTaskDefinition(TASK_NAME, td);
+
+                MessageBox.Show("Tâche planifiée créée avec succès !");
+            }
+        }
+
+        private void checkTask_Click(object sender, EventArgs e)
+        {
+            if (TaskExists(TASK_NAME))
+            {
+                MessageBox.Show("The task is already exist's");
+            }
+            else
+            {
+                MessageBox.Show("The task do not exists");
+            }
+        }
+
+        private bool TaskExists(string taskName)
+        {
+            using (TaskService ts = new TaskService())
+            {
+                Task task = ts.GetTask(taskName);
+                return task != null;
+            }
+        }
+
+        private void githubProject_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Piarre/Temped");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
