8b1f430383007ab4e0c4bdc4564c5fedb15c8988
Piarre <pierre@ideestore.fr>
2023-09-03 11:10:47 +0200

    Misc

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,8 @@
             this.enableTempFolder = new System.Windows.Forms.Button();
             this.githubProject = new System.Windows.Forms.LinkLabel();
             this.checkTask = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.forSpecificUser = new System.Windows.Forms.CheckBox();
+            this.usersList = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // enableTempFolder
@@ -47,7 +48,7 @@
             // githubProject
             // 
             this.githubProject.AutoSize = true;
-            this.githubProject.Location = new System.Drawing.Point(13, 167);
+            this.githubProject.Location = new System.Drawing.Point(12, 170);
             this.githubProject.Name = "githubProject";
             this.githubProject.Size = new System.Drawing.Size(73, 13);
             this.githubProject.TabIndex = 3;
@@ -65,23 +66,34 @@
             this.checkTask.UseVisualStyleBackColor = true;
             this.checkTask.Click += new System.EventHandler(this.checkTask_Click);
             // 
-            // label1
+            // forSpecificUser
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.forSpecificUser.AutoSize = true;
+            this.forSpecificUser.Location = new System.Drawing.Point(12, 12);
+            this.forSpecificUser.Name = "forSpecificUser";
+            this.forSpecificUser.Size = new System.Drawing.Size(132, 17);
+            this.forSpecificUser.TabIndex = 6;
+            this.forSpecificUser.Text = "Run for a specific user";
+            this.forSpecificUser.UseVisualStyleBackColor = true;
+            this.forSpecificUser.CheckedChanged += new System.EventHandler(this.forSpecificUser_CheckedChanged);
+            // 
+            // usersList
+            // 
+            this.usersList.FormattingEnabled = true;
+            this.usersList.Location = new System.Drawing.Point(150, 10);
+            this.usersList.Name = "usersList";
+            this.usersList.Size = new System.Drawing.Size(116, 21);
+            this.usersList.TabIndex = 7;
             // 
             // Temped
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(283, 192);
+            this.Controls.Add(this.usersList);
+            this.Controls.Add(this.forSpecificUser);
             this.Controls.Add(this.checkTask);
             this.Controls.Add(this.githubProject);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.enableTempFolder);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -98,7 +110,8 @@
         private System.Windows.Forms.Button enableTempFolder;
         private System.Windows.Forms.LinkLabel githubProject;
         private System.Windows.Forms.Button checkTask;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox forSpecificUser;
+        private System.Windows.Forms.ComboBox usersList;
     }
 }
 
